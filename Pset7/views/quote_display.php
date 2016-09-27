e71063a8420f4596d8263590237ab0b6f75a8d80
Kevin Voshell <kevinvoshell@gmail.com>
2016-09-27 15:01:52 -0500

    Create quote_display.php

---
+++ Pset7/views/quote_display.php
@@ -0,0 +1,6 @@
+<p>
+    The price for one share of <strong><?=$name?></strong> <?="($symbol)"?> is <strong>$<?=number_format($price, 2, '.', '')?></strong>
+</p>
+<div>
+    <a href="../index.php">Return Home</a>
+</div>
