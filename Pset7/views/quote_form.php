30efb076cc7583645b270d768c1375c9546e9ac9
Kevin Voshell <kevinvoshell@gmail.com>
2016-09-27 15:02:14 -0500

    Create quote_form.php

---
+++ Pset7/views/quote_form.php
@@ -0,0 +1,16 @@
+<form action="quote.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus="form-control" autocomplete="off" name="symbol" placeholder="Stock Symbol" type="text"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                Get Quote
+            </button>
+        </div>
+    </fieldset>
+</form>
+<div>
+    or <a href="../index.php">Return Home</a>
+</div>
