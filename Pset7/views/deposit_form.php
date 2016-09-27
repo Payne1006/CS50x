03874186a642d4a197323578e569b61fc93878e2
Kevin Voshell <kevinvoshell@gmail.com>
2016-09-27 14:47:24 -0500

    Create deposit_form.php

---
+++ Pset7/views/deposit_form.php
@@ -0,0 +1,19 @@
+<form action="deposit.php" method="post">
+    <div>
+        <h5>
+            Please enter an amount to deposit:
+        </h5>
+    </div>
+    <div class="form-group">
+        $ <input autofocus name="deposit" placeholder="Enter amount" type="text">
+    </div> 
+    <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                Deposit Funds
+            </button>
+    </div>
+</form>
+    <div>
+        <a href="../index.php">Return Home</a>
+    </div>
